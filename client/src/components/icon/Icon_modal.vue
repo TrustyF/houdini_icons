fc0d3e4d6a52e0c63d55d03d71630d5e4a4b982c
TrustyF <sirjacobs.arthur@gmail.com>
2026-08-15 09:56:11 -0700

    Fixed broken clipboard link

--- client/src/components/icon/Icon_modal.vue
+++ client/src/components/icon/Icon_modal.vue
@@ -15,7 +15,7 @@ let emits = defineEmits(['close'])
 let alert_content = inject('alert_content')
 
 function add_to_clipboard() {
-  let clip = `hicon:/SVGIcons.index?${props['data']['category']['name']}_${props['data']['name']['name']}.svg`
+  let clip = `hicon:/SVGIcons.index?${props['data']['category']}_${props['data']['name']}.svg`
   navigator.clipboard.writeText(clip);
 
   alert_content.value = {
@@ -25,8 +25,8 @@ function add_to_clipboard() {
 }
 
 function download_svg(data) {
-  let file = data['name']['name']
-  let category = data['category']['name']
+  let file = data['name']
+  let category = data['category']
   let path
 
   if (category.length < 1 || category === 'icons') path = `/icons/${file}.svg`
